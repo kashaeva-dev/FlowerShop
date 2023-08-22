a921773d57ec8c0b89276a2a0794596271a10613
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-08-22 12:14:26 +0300

    Add models

--- shopbot/models.py
+++ shopbot/models.py
@@ -1,3 +1,275 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название кампании', null=True)
+    refer_id = models.PositiveIntegerField(verbose_name='Telegram ID кампании')
+    refer_url = models.URLField(verbose_name='Ссылка на кампанию')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Рекламная кампания'
+        verbose_name_plural = 'Рекламные кампании'
+
+
+class User(models.Model):
+    telegram_id = models.PositiveBigIntegerField(verbose_name='Telegram ID')
+    first_name = models.CharField(max_length=40, verbose_name='Имя', null=True)
+    created_at = models.DateTimeField(verbose_name='Создано', auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Client(User):
+    personal_data_consent = models.BooleanField(verbose_name='Согласие на обработку персональных данных', default=False)
+    advertisement = models.ForeignKey(Advertisement,
+                                      on_delete=models.PROTECT,
+                                      verbose_name='Рекламная кампания',
+                                      null=True,
+                                      blank=True,
+                                      related_name='clients',
+                                      )
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+    def __str__(self):
+        return f'{self.chat_id}: {self.first_name}'
+
+
+class Staff(User):
+    STAFF_ROLE_CHOICES = (
+        ('admin', 'Админ'),
+        ('florist', 'Флорист'),
+        ('currier', 'Курьер'),
+    )
+    last_name = models.CharField(max_length=40, verbose_name='Фамилия')
+    role = models.CharField(max_length=40, verbose_name='Роль', choices=STAFF_ROLE_CHOICES)
+    start_date = models.DateTimeField(verbose_name='Дата начала работы', auto_now_add=True)
+    end_date = models.DateTimeField(verbose_name='Дата увольнения', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+    def __str__(self):
+        return f'{self.chat_id}: {self.first_name} {self.last_name}'
+
+
+class Occasion(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Повод')
+
+    class Meta:
+        verbose_name = 'Повод'
+        verbose_name_plural = 'Поводы'
+
+    def __str__(self):
+        return self.name
+
+
+class Colors(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Цвет')
+
+    class Meta:
+        verbose_name = 'Цвет'
+        verbose_name_plural = 'Цвета'
+
+    def __str__(self):
+        return self.name
+
+
+class Gamma(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Гамма')
+
+    class Meta:
+        verbose_name = 'Гамма'
+        verbose_name_plural = 'Гаммы'
+
+    def __str__(self):
+        return self.name
+
+
+class Flower(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Название цветка')
+    color = models.ForeignKey(Colors,
+                              on_delete=models.PROTECT,
+                              verbose_name='Цвет',
+                              related_name='flowers',
+                              )
+
+    class Meta:
+        verbose_name = 'Цветок'
+        verbose_name_plural = 'Цветы'
+
+    def __str__(self):
+        return self.name
+
+
+class Bouquet(models.Model):
+    image = models.ImageField(verbose_name='Изображение', upload_to='bouquets')
+    name = models.CharField(max_length=40, verbose_name='Название букета')
+    flowers = models.ManyToManyField(Flower,
+                                    through='Composition',
+                                    verbose_name='Цветы',
+                                    related_name='bouquets',
+                                    )
+    meaning = models.TextField(verbose_name='Описание')
+    occasion = models.ManyToManyField(Occasion,
+                                      verbose_name='Повод',
+                                      related_name='bouquets',
+                                      )
+    price = models.PositiveIntegerField(verbose_name='Цена')
+
+    class Meta:
+        verbose_name = 'Букет'
+        verbose_name_plural = 'Букеты'
+
+    def __str__(self):
+        return self.name
+
+
+class Composition(models.Model):
+    bouquet = models.ForeignKey(Bouquet,
+                                on_delete=models.PROTECT,
+                                verbose_name='Букет',
+                                related_name='composition',
+                                )
+    flower = models.ForeignKey(Flower,
+                               on_delete=models.PROTECT,
+                               verbose_name='Цветок',
+                               related_name='composition',
+                               )
+    quantity = models.PositiveIntegerField(verbose_name='Количество')
+
+    class Meta:
+        verbose_name = 'Состав'
+        verbose_name_plural = 'Составы'
+
+    def __str__(self):
+        return f'{self.bouquet.name}: {self.flower.name} - {self.quantity} шт.'
+
+
+class ConsultingStatus(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Статус')
+
+    class Meta:
+        verbose_name = 'Статус консультации'
+        verbose_name_plural = 'Статусы консультаций'
+
+    def __str__(self):
+        return self.name
+
+
+class Consulting(models.Model):
+    client = models.ForeignKey(Client,
+                               on_delete=models.PROTECT,
+                               verbose_name='Клиент',
+                               related_name='consultings',
+                               )
+    florist = models.ForeignKey(Staff,
+                                on_delete=models.PROTECT,
+                                verbose_name='Флорист',
+                                limit_choices_to={'role': 'florist'},
+                                related_name='consultings',
+                                )
+    status = models.ManyToManyField(ConsultingStatus,
+                                    through='ConsultingStatusHistory',
+                                    )
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
+    contact_phone = models.CharField(max_length=40, verbose_name='Контактный телефон')
+    first_call_at = models.DateTimeField(verbose_name='Дата и время звонка', null=True, blank=True)
+    occasion = models.ForeignKey(Occasion,
+                                 on_delete=models.PROTECT,
+                                 verbose_name='Повод',
+                                 related_name='consultings',
+                                 )
+    occasion_text = models.CharField(max_length=40, verbose_name='Повод (уточнение)', blank=True)
+    gamma = models.ForeignKey(Gamma,
+                              on_delete=models.PROTECT,
+                              verbose_name='Гамма',
+                              related_name='consultings',
+                              )
+
+    class Meta:
+        verbose_name = 'Консультация'
+        verbose_name_plural = 'Консультации'
+
+    def __str__(self):
+        return f'{self.client}: {self.florist}'
+
+
+class ConsultingStatusHistory(models.Model):
+    consulting = models.ForeignKey(Consulting,
+                                   on_delete=models.PROTECT,
+                                   verbose_name='Консультация',
+                                   related_name='status_history',
+                                   )
+    status = models.ForeignKey(ConsultingStatus,
+                               on_delete=models.PROTECT,
+                               verbose_name='Статус',
+                               related_name='status_history',
+                               )
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'История статусов консультации'
+        verbose_name_plural = 'Истории статусов консультаций'
+
+    def __str__(self):
+        return f'{self.consulting}: {self.status}'
+
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('new', 'Новый'),
+        ('processing', 'В обработке'),
+        ('ready', 'Готов'),
+        ('delivered', 'Доставлен'),
+        ('canceled', 'Отменен'),
+    )
+    client = models.ForeignKey(Client,
+                               on_delete=models.PROTECT,
+                               verbose_name='Клиент',
+                               related_name='orders',
+                               )
+    bouquet = models.ForeignKey(Bouquet,
+                                on_delete=models.PROTECT,
+                                verbose_name='Букет',
+                                related_name='orders',
+                                )
+    consultation = models.ForeignKey(Consulting,
+                                     on_delete=models.PROTECT,
+                                     verbose_name='Консультация',
+                                     related_name='orders',
+                                     null=True,
+                                     blank=True,
+                                     )
+    status = models.CharField(max_length=40, verbose_name='Статус', choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
+    delivery_date = models.DateTimeField(verbose_name='Дата доставки')
+    delivery_address = models.CharField(max_length=200, verbose_name='Адрес доставки')
+    contact_phone = models.CharField(max_length=20, verbose_name='Контактный телефон')
+    contact_name = models.CharField(max_length=40, verbose_name='Контактное лицо')
+    currier = models.ForeignKey(Staff,
+                                on_delete=models.PROTECT,
+                                verbose_name='Курьер',
+                                limit_choices_to={'role': 'currier'},
+                                related_name='orders',
+                                null=True,
+                                blank=True,
+                                )
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return f'{self.bouquet.name}: ' \
+               f'{self.delivery_date} по адресу: {self.delivery_address} - {self.status}'
