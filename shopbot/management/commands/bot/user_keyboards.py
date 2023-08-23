355f7db0f8c0cbdafb88fa704eea7de19d0dc074
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-08-23 18:19:06 +0300

    Fix catalog pagination bug

--- shopbot/management/commands/bot/user_keyboards.py
+++ shopbot/management/commands/bot/user_keyboards.py
@@ -1,17 +1,21 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from asgiref.sync import sync_to_async
+
+from shopbot.models import Bouquet
 
 
 async def get_catalog_keyboard(bouquet_id: int):
-    # TODO: нужно исправить алгоритм пагинации
-    next_bouquet_id = (int(bouquet_id) + 1) % 7
+    bouquet_number = await sync_to_async(Bouquet.objects.count)()
+    next_bouquet_id = (int(bouquet_id) + 1) % bouquet_number
     if next_bouquet_id == 0:
-        next_bouquet_id = 1
-    prev_bouquet_id = (int(bouquet_id) + 7 - 1) % 7
+        next_bouquet_id = bouquet_number
+    prev_bouquet_id = (int(bouquet_id) - 1) % bouquet_number
     if prev_bouquet_id == 0:
-        prev_bouquet_id = 7
+        prev_bouquet_id = 1
     inline_keyboard = [
         [
             InlineKeyboardButton(text='<<', callback_data=f'catalog_{prev_bouquet_id}'),
+            InlineKeyboardButton(text=f'{bouquet_id} из {bouquet_number}', callback_data='no_action'),
             InlineKeyboardButton(text='>>', callback_data=f'catalog_{next_bouquet_id}'),
         ],
         [
